aaf4fd26ed2489f546f00878d59973a7a1960fe0
earlchristianb <35775321+earlchristianb@users.noreply.github.com>
2022-02-24 02:42:58 +0800

    before setting up db

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from "@nestjs/common";
+
+
+@Injectable({})
+export class AuthService{
+
+   
+
+    signup() {
+        return {message:'You are signed up', success:true}
+    }
+    
+    signin() {
+        return {message:'You are signed in', success:true}
+    }
+}
---
+++ src/post/post.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from "@nestjs/common";
+
+@Injectable({})
+export class PostService{
+
+    getPost() {
+        return 'all post'
+    }
+}
