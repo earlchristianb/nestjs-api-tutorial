6b48fa81321b2c687a89c6423b42ef4ebcba3b5e
earlchristianb <35775321+earlchristianb@users.noreply.github.com>
2022-02-27 18:52:35 +0800

    .

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,6 +1,5 @@
 import { Global, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { resolveConfigFile } from 'prettier';
 import { PrismaService } from './prisma.service';
 
 @Global()
